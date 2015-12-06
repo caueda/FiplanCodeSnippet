d13bee3c20b4eb5e9022a6e870477a33f39e1d35
caueda <caueda@matrix>
2015-12-06 08:20:08 -0300

    First version.

---
+++ br.gov.mt.fiplan.plugin.FiplanCodeSnippet/src/br/gov/mt/fiplan/plugin/fiplancodesnippet/views/FiplanSnippetView.java
@@ -0,0 +1,316 @@
+package br.gov.mt.fiplan.plugin.fiplancodesnippet.views;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+
+/**
+ * This sample class demonstrates how to plug-in a new
+ * workbench view. The view shows data obtained from the
+ * model. The sample creates a dummy model on the fly,
+ * but a real implementation would connect to the model
+ * available either in this or another plug-in (e.g. the workspace).
+ * The view is connected to the model using a content provider.
+ * <p>
+ * The view uses a label provider to define how model
+ * objects should be presented in the view. Each
+ * view can present the same model objects using
+ * different labels and icons, if needed. Alternatively,
+ * a single label provider can be shared between views
+ * in order to ensure that objects of the same type are
+ * presented in the same way everywhere.
+ * <p>
+ */
+
+public class FiplanSnippetView extends ViewPart {
+
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "br.com.plugin.study.views.SampleView";
+
+	private TreeViewer viewer;
+	private Action action1;
+	private Action action2;
+	private Action doubleClickAction;
+	private Map<String, String[]> tags = new HashMap<String, String[]>();
+
+	{
+		tags.put("HTML", new String[]{"<body>\n</body>", "<input type=\"text\" name=\"name\"/>", "<a href=\"\">Link</a>"});
+		tags.put("jQuery", new String[]{
+				"$(\"id\").val()",
+				"	$(document).ready(function(){\n" + 
+				"	//TODO\n" + 
+				"	});",
+				"Localizar","Replace","Formatar","Refatorar"});		
+	}
+	/*
+	 * The content provider class is responsible for
+	 * providing objects to the view. It can wrap
+	 * existing objects in adapters or simply return
+	 * objects as-is. These objects may be sensitive
+	 * to the current input of the view, or ignore
+	 * it and always show the same content 
+	 * (like Task List, for example).
+	 */
+	 
+	class ViewContentProvider extends LabelProvider implements ITreeContentProvider{
+		public String getText(Object element){
+			if(element instanceof Map){
+				return "Menu";
+			} else if(element instanceof Map.Entry){
+				return ((Map.Entry) element).getKey().toString();				
+			} else if(element instanceof String){
+				return element.toString();
+			} else {
+				return "Elemento desconhecido: " + element.getClass();
+			}
+		}
+		
+
+		@Override
+		public void dispose() {
+			
+		}
+
+		@Override
+		public void inputChanged(Viewer arg0, Object arg1, Object arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Object[] getChildren(Object parent) {
+			if(parent instanceof Map){
+				return ((Map)parent).entrySet().toArray();
+			} else if(parent instanceof Map.Entry){
+				return getChildren(((Map.Entry)parent).getValue());
+			} else if(parent instanceof String[]){
+				return (String[])parent;
+			} else {
+				return new String[0];
+			}
+		}
+
+		@Override
+		public Object[] getElements(Object inputElement) {
+			if(inputElement instanceof Object[]){
+				return ((Object[]) inputElement);
+			} else {
+				return new Object[0];
+			}
+		}
+
+		@Override
+		public Object getParent(Object arg0) {
+			return null;
+		}
+
+		@Override
+		public boolean hasChildren(Object element) {
+			if(element instanceof Map){
+				return !((Map)element).isEmpty();
+			} else if(element instanceof Map.Entry){
+				return hasChildren(((Map.Entry)element).getValue());
+			} else if(element instanceof String[]){
+				return (element != null && ((String[])element).length > 0);
+			} else {
+				return false;
+			}
+		}
+
+
+		@Override
+		public Image getImage(Object element) {
+			if(element instanceof Map.Entry){
+				return PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJ_FOLDER);
+			} else 
+				return super.getImage(element);
+		}
+		
+		
+	}
+	
+	class NameSorter extends ViewerSorter {
+	}
+
+	/**
+	 * The constructor.
+	 */
+	public FiplanSnippetView() {
+	}
+
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {		
+		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new ViewContentProvider());
+		//viewer.setSorter(new NameSorter());
+		viewer.setInput(new Object[]{tags});
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+			
+			@Override
+			public void doubleClick(DoubleClickEvent event) {
+				TreeSelection selection = (TreeSelection)event.getSelection();				
+				Object obj = selection.getFirstElement();
+				
+				try {
+					ITextSelection content = (ITextSelection)
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+						.getActivePage().getActiveEditor().getEditorSite().getSelectionProvider()
+						.getSelection();
+					
+					IEditorPart part = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+							.getActivePage().getActiveEditor();
+					ITextEditor editor = (ITextEditor)part;
+					IDocumentProvider provider = editor.getDocumentProvider();
+					IDocument document = provider.getDocument(editor.getEditorInput());
+					ISelection sel = editor.getSelectionProvider().getSelection();
+					
+					if(sel instanceof TextSelection) {
+						final TextSelection text = (TextSelection)sel;
+						try {							
+							document.replace( text.getOffset(), text.getLength(), obj.toString() );
+						} catch(Exception e) {
+							e.printStackTrace();
+						}
+					}
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// Create the help context id for the viewer's control
+//		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "br.com.plugin.Study.viewer");
+//		getSite().setSelectionProvider(viewer);
+//		makeActions();
+//		hookContextMenu();
+//		hookDoubleClickAction();
+//		contributeToActionBars();
+	}
+
+	private void hookContextMenu() {
+		MenuManager menuMgr = new MenuManager("#PopupMenu");
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				FiplanSnippetView.this.fillContextMenu(manager);
+			}
+		});
+		Menu menu = menuMgr.createContextMenu(viewer.getControl());
+		viewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuMgr, viewer);
+	}
+
+	private void contributeToActionBars() {
+		IActionBars bars = getViewSite().getActionBars();
+		fillLocalPullDown(bars.getMenuManager());
+		fillLocalToolBar(bars.getToolBarManager());
+	}
+
+	private void fillLocalPullDown(IMenuManager manager) {
+		manager.add(action1);
+		manager.add(new Separator());
+		manager.add(action2);
+	}
+
+	private void fillContextMenu(IMenuManager manager) {
+		manager.add(action1);
+		manager.add(action2);
+		// Other plug-ins can contribute there actions here
+		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
+	}
+	
+	private void fillLocalToolBar(IToolBarManager manager) {
+		manager.add(action1);
+		manager.add(action2);
+	}
+
+	private void makeActions() {
+		action1 = new Action() {
+			public void run() {
+				showMessage("Action 1 executed");
+			}
+		};
+		action1.setText("Action 1");
+		action1.setToolTipText("Action 1 tooltip");
+		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		
+		action2 = new Action() {
+			public void run() {
+				showMessage("Action 2 executed");
+			}
+		};
+		action2.setText("Action 2");
+		action2.setToolTipText("Action 2 tooltip");
+		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		doubleClickAction = new Action() {
+			public void run() {
+				ISelection selection = viewer.getSelection();
+				Object obj = ((IStructuredSelection)selection).getFirstElement();
+				showMessage("Double-click detected on "+obj.toString());
+			}
+		};
+	}
+
+	private void hookDoubleClickAction() {
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent event) {
+				doubleClickAction.run();
+			}
+		});
+	}
+	private void showMessage(String message) {
+		MessageDialog.openInformation(
+			viewer.getControl().getShell(),
+			"Sample View",
+			message);
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+}
