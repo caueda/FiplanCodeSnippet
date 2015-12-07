ca62c9f3ef2edc3726cb548f49d023df0c65b043
caueda <caueda@matrix>
2015-12-07 09:08:44 -0300

    For tests it was added the interface IStyledLabelProvider.

--- br.gov.mt.fiplan.plugin.FiplanCodeSnippet/src/br/gov/mt/fiplan/plugin/fiplancodesnippet/views/FiplanSnippetView.java
+++ br.gov.mt.fiplan.plugin.FiplanCodeSnippet/src/br/gov/mt/fiplan/plugin/fiplancodesnippet/views/FiplanSnippetView.java
@@ -14,12 +14,15 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
@@ -37,24 +40,6 @@ import org.eclipse.ui.part.ViewPart;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
-
-/**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
- */
 
 public class FiplanSnippetView extends ViewPart {
 
@@ -78,17 +63,8 @@ public class FiplanSnippetView extends ViewPart {
 				"	});",
 				"Localizar","Replace","Formatar","Refatorar"});		
 	}
-	/*
-	 * The content provider class is responsible for
-	 * providing objects to the view. It can wrap
-	 * existing objects in adapters or simply return
-	 * objects as-is. These objects may be sensitive
-	 * to the current input of the view, or ignore
-	 * it and always show the same content 
-	 * (like Task List, for example).
-	 */
 	 
-	class ViewContentProvider extends LabelProvider implements ITreeContentProvider{
+	class ViewContentProvider extends LabelProvider implements ITreeContentProvider, IStyledLabelProvider {
 		public String getText(Object element){
 			if(element instanceof Map){
 				return "Menu";
@@ -161,6 +137,17 @@ public class FiplanSnippetView extends ViewPart {
 			} else 
 				return super.getImage(element);
 		}
+
+
+		@Override
+		public StyledString getStyledText(Object element) {
+			String text = getText(element);
+			StyledString ss = new StyledString(text);
+			if(element instanceof String) {
+				ss.append("", StyledString.DECORATIONS_STYLER);
+			}
+			return ss;
+		}
 		
 		
 	}
@@ -174,14 +161,10 @@ public class FiplanSnippetView extends ViewPart {
 	public FiplanSnippetView() {
 	}
 
-	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
-	 */
 	public void createPartControl(Composite parent) {		
 		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
 		viewer.setContentProvider(new ViewContentProvider());
-		viewer.setLabelProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new DelegatingStyledCellLabelProvider(new ViewContentProvider()));
 		//viewer.setSorter(new NameSorter());
 		viewer.setInput(new Object[]{tags});
 		viewer.addDoubleClickListener(new IDoubleClickListener() {
